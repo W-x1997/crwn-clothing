cfc10375eb762d76be46a5c6d91386df105f2e3a
wx <421980589@qq.com>
2021-01-05 13:10:41 -0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TopicsList = props => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  return (
+    <div>
+      <h1>TOPIC Details</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route  exact path='/' component={HomePage}/>
+        <Route  path='/shop/hats' component={TopicsList}/>
+        <Route  path='/topics/:topicId' component={TopicDetail} />
+      </Switch>
+      
     </div>
   );
 }
